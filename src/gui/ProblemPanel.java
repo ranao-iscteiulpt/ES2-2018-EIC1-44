27ec28b7a3243c9f7b2ab2587f8b4ffe0c19db5f
cammo1-iscteiul <cammo1@iscte-iul.pt>
2018-03-19 10:11:18 +0000

    botao adiciona variaveis

--- src/gui/ProblemPanel.java
+++ src/gui/ProblemPanel.java
@@ -1,35 +1,25 @@
 package gui;
 
-import javax.swing.JPanel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JLabel;
-import javax.swing.SwingConstants;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
 import javax.swing.table.DefaultTableModel;
 
 import xml.XMLOperations;
 
-import javax.swing.JTextPane;
-import java.awt.GridBagLayout;
-import javax.swing.JTextField;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.GridLayout;
-import javax.swing.JCheckBox;
-import javax.swing.BoxLayout;
-import javax.swing.DefaultListModel;
-import javax.swing.SpringLayout;
-import javax.swing.JScrollBar;
-import javax.swing.JScrollPane;
-import javax.swing.JList;
-import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
-import javax.swing.JButton;
-import javax.swing.JTextArea;
-import javax.swing.JTable;
-import java.awt.ScrollPane;
+
 
 public class ProblemPanel extends JPanel {
+
 	private JTextField titleTextField;
 	private JTextField waitTimeTextField;
 	private JTextField invalidValueTextField;
@@ -37,16 +27,19 @@ public class ProblemPanel extends JPanel {
 	private DefaultTableModel model = new DefaultTableModel();
 	private XMLOperations xml = new XMLOperations();
 	private JTable table = new JTable(model);
-	private String[] columns = {"Title","Tipo", "Valor"};
+	//private String[] columns = {"Title","Tipo", "Valor"};
 
 	/**
 	 * Create the panel.
 	 */
 	public ProblemPanel() {
 
-
 		SpringLayout springLayout = new SpringLayout();
 		setLayout(springLayout);
+		
+		model.addColumn("Name");
+		model.addColumn("Type");
+		model.addColumn("Value");
 
 		JLabel lblNewLabel = new JLabel("Title");
 		springLayout.putConstraint(SpringLayout.NORTH, lblNewLabel, 34, SpringLayout.NORTH, this);
@@ -62,28 +55,25 @@ public class ProblemPanel extends JPanel {
 		titleTextField.setColumns(10);
 
 		JLabel lblVariabels = new JLabel("Variables");
-		springLayout.putConstraint(SpringLayout.SOUTH, lblVariabels, -332, SpringLayout.SOUTH, this);
+		springLayout.putConstraint(SpringLayout.WEST, lblVariabels, 35, SpringLayout.WEST, this);
+		springLayout.putConstraint(SpringLayout.SOUTH, lblVariabels, -346, SpringLayout.SOUTH, this);
 		add(lblVariabels);
 
 		JLabel lblWaitTime = new JLabel("Wait Time");
-		springLayout.putConstraint(SpringLayout.WEST, lblWaitTime, 35, SpringLayout.WEST, this);
-		springLayout.putConstraint(SpringLayout.WEST, lblVariabels, 0, SpringLayout.WEST, lblWaitTime);
 		add(lblWaitTime);
 
 		waitTimeTextField = new JTextField();
-		springLayout.putConstraint(SpringLayout.NORTH, waitTimeTextField, -3, SpringLayout.NORTH, lblWaitTime);
-		springLayout.putConstraint(SpringLayout.WEST, waitTimeTextField, 27, SpringLayout.EAST, lblWaitTime);
-		springLayout.putConstraint(SpringLayout.EAST, waitTimeTextField, 0, SpringLayout.EAST, lblNewLabel);
+		springLayout.putConstraint(SpringLayout.WEST, waitTimeTextField, 24, SpringLayout.EAST, lblWaitTime);
+		springLayout.putConstraint(SpringLayout.NORTH, lblWaitTime, 3, SpringLayout.NORTH, waitTimeTextField);
 		add(waitTimeTextField);
 		waitTimeTextField.setColumns(10);
 
 		JLabel lblInvalidValue = new JLabel("Invalid Value");
-		springLayout.putConstraint(SpringLayout.NORTH, lblInvalidValue, 0, SpringLayout.NORTH, lblWaitTime);
+		springLayout.putConstraint(SpringLayout.EAST, waitTimeTextField, -73, SpringLayout.WEST, lblInvalidValue);
 		springLayout.putConstraint(SpringLayout.EAST, lblInvalidValue, -208, SpringLayout.EAST, this);
 		add(lblInvalidValue);
 
 		invalidValueTextField = new JTextField();
-		springLayout.putConstraint(SpringLayout.NORTH, invalidValueTextField, -3, SpringLayout.NORTH, lblWaitTime);
 		springLayout.putConstraint(SpringLayout.WEST, invalidValueTextField, 19, SpringLayout.EAST, lblInvalidValue);
 		springLayout.putConstraint(SpringLayout.EAST, invalidValueTextField, -89, SpringLayout.EAST, this);
 		add(invalidValueTextField);
@@ -100,35 +90,48 @@ public class ProblemPanel extends JPanel {
 		springLayout.putConstraint(SpringLayout.SOUTH, descriptionTextArea, -59, SpringLayout.SOUTH, this);
 		springLayout.putConstraint(SpringLayout.EAST, descriptionTextArea, -44, SpringLayout.EAST, this);
 		add(descriptionTextArea);
-		
+
 
 
 		JLabel lblDescriptioni = new JLabel("Description");
+		springLayout.putConstraint(SpringLayout.EAST, lblWaitTime, 0, SpringLayout.EAST, lblDescriptioni);
 		springLayout.putConstraint(SpringLayout.NORTH, lblDescriptioni, 253, SpringLayout.NORTH, this);
-		springLayout.putConstraint(SpringLayout.SOUTH, lblWaitTime, -23, SpringLayout.NORTH, lblDescriptioni);
 		springLayout.putConstraint(SpringLayout.NORTH, descriptionTextArea, 6, SpringLayout.SOUTH, lblDescriptioni);
 		springLayout.putConstraint(SpringLayout.WEST, lblDescriptioni, 32, SpringLayout.WEST, this);
 		add(lblDescriptioni);
-		
-		model.addColumn("Name");
-		model.addColumn("Type");
-		model.addColumn("Value");
-		
-		model.addRow(new Object[]{"1","2","3"});
-		model.addRow(new Object[]{"4","5","6"});				
-		
+
+
+
+			
+
 		JScrollPane scrollPane = new JScrollPane(table);
+		springLayout.putConstraint(SpringLayout.SOUTH, scrollPane, -221, SpringLayout.SOUTH, this);
+		springLayout.putConstraint(SpringLayout.NORTH, invalidValueTextField, 6, SpringLayout.SOUTH, scrollPane);
+		springLayout.putConstraint(SpringLayout.NORTH, lblInvalidValue, 9, SpringLayout.SOUTH, scrollPane);
+		springLayout.putConstraint(SpringLayout.NORTH, waitTimeTextField, 6, SpringLayout.SOUTH, scrollPane);
 		springLayout.putConstraint(SpringLayout.NORTH, scrollPane, 6, SpringLayout.SOUTH, lblVariabels);
-		springLayout.putConstraint(SpringLayout.SOUTH, scrollPane, -28, SpringLayout.NORTH, waitTimeTextField);
 		springLayout.putConstraint(SpringLayout.WEST, scrollPane, 35, SpringLayout.WEST, this);
 		springLayout.putConstraint(SpringLayout.EAST, scrollPane, -44, SpringLayout.EAST, this);
 		add(scrollPane);
+
+		JButton addVariableButton = new JButton("+");
+		springLayout.putConstraint(SpringLayout.WEST, addVariableButton, 10, SpringLayout.EAST, lblVariabels);
+		springLayout.putConstraint(SpringLayout.SOUTH, addVariableButton, 0, SpringLayout.SOUTH, lblVariabels);
+		add(addVariableButton);
+
+		addVariableButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+				model.addRow(new Object[]{"","",""});
+			}
+		});
 
 		btnDone.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-
 				xml.writeXML(titleTextField.getText(), model ,descriptionTextArea.getText(), waitTimeTextField.getText(), invalidValueTextField.getText());
 
 				JOptionPane.showMessageDialog(btnDone, "XML Created");
@@ -137,4 +140,6 @@ public class ProblemPanel extends JPanel {
 		});
 
 	}
+
 }
+
