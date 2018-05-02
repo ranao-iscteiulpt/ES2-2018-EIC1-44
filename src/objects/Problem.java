5219e2e5ab884e29fa4bf0de3492256452e2e3d7
ranao-iscteiulpt <ranao@iscte-iul.pt>
2018-05-02 17:52:15 +0100

    correcao bug combo box tipo de variavel valor string para double

--- src/objects/Problem.java
+++ src/objects/Problem.java
@@ -1,19 +1,23 @@
 package objects;
 
+import java.util.ArrayList;
+
 public class Problem {
 
+	private ArrayList<Variable> variableList;
 	private String name;
 	private String description;
 	private int waitTime;
 	private int invalidValue;
 	private int variableCounter;
 
-	public Problem(String name, String description, int waitTime, int invalidValue, int variableCounter) {
+	public Problem(String name, String description, int waitTime, int invalidValue, int variableCounter,ArrayList variableList) {
 		this.name = name;
 		this.description = description;
 		this.waitTime = waitTime;
 		this.invalidValue = invalidValue;
 		this.variableCounter = variableCounter;
+		this.variableList = variableList;
 	}
 	
 	public String getName() {
